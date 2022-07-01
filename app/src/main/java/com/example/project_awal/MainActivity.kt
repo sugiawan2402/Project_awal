973934f2a3ed8b110b5ee68eb591718d0f1f8c0b
Sugiawan <suksesfulman02@gmail.com>
2022-07-01 20:33:44 +0800

    Initial commit

---
+++ app/src/main/java/com/example/project_awal/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.project_awal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var imgt:ImageView
+        var imgK:ImageView
+        var imgLam:ImageView
+
+        imgt = findViewById(R.id.imgt)
+        imgK = findViewById(R.id.imgK)
+        imgLam = findViewById(R.id.imgLam)
+
+        imgLam.set
+
+        }
+
+
+    }
+}
